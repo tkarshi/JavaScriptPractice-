ea14dce7a277deb662006d3ea028ae6c0b5cb06d
tkarshi <tkarshi2000@gmail.com>
2024-07-09 10:25:10 +0530

    When Click the button the text of button is changed

---
+++ index.js
@@ -0,0 +1,10 @@
+//check when click the button the alert message is get or not
+// function btn_Click() {
+//   alert("button clicked");
+// }
+
+//Change the button text when click the button
+
+function btn_Click() {
+  document.getElementById("MyBtn").innerHTML = "Button Clicked";
+}
